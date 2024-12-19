21a68b6d22cb81c4de87c5cd970eb1bea150f16f
steiner112 <asvmusic26@gmail.com>
2024-12-19 09:29:39 +0530

    update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect , useState } from "react";
-import axios from "axios"
+import React, { useEffect } from "react";
+// import axios from "axios"
 
 export default function Projects() {
-const [data , setData] = useState([])
+// const [data , setData] = useState([])
 
 function animate() {
   document.querySelector('#card-box').style.animation = "Box1 1s forwards";
 }
 
 
-useEffect(()=>{
-axios.get('http://localhost:9090/Project')
-.then((resp)=>setData(resp.data))
-.catch((err)=>console.log(err))
-},[])
+// useEffect(()=>{
+// axios.get('http://localhost:9090/Project')
+// .then((resp)=>setData(resp.data))
+// .catch((err)=>console.log(err))
+// },[])
 
 useEffect(() => {
   // Run animation when data has been updated and the component has re-rendered
@@ -21,13 +21,15 @@ useEffect(() => {
   // if (data.length <= 0) {
   //   document.querySelector('#loading').innerHTML = "loading..."
   // }
-  if(data.length > 0){
-    document.querySelector('#loading').style.display = "none"
 
-    animate();
+  animate();
+  // if(data.length > 0){
+  //   document.querySelector('#loading').style.display = "none"
 
-  }
-}, [data]); // Dependency on `data` means this runs every time `data` is updated
+  
+
+  // }
+}); // Dependency on `data` means this runs every time `data` is updated
 
 
   return (
@@ -35,14 +37,21 @@ useEffect(() => {
     <main className='homeFlex1221'>
       <h1 id="loading">loading...</h1>
       <span id="card-box">
-        { data.map((item)=>{
-    return <a href={item.Link}>< div key={item._id} id='card'>
-     <img src={item.Img} alt="image-project" />
-        <h1 >{item.Name}</h1>
+        
+     <a href="https://liveworldsweather.netlify.app/">< div id='card'>
+     <img src="https://dl.memuplay.com/new_market/img/com.appsinnova.weatherforecast.liveweather.icon.2023-09-15-22-20-27.png" alt="image-project" />
+        <h1>Live Weather</h1>
       </div>
       </a>
-      })
-      }
+
+
+      <a href="https://getitnoww.netlify.app/">< div id='card'>
+     <img src="https://media.istockphoto.com/id/1304570729/vector/front-end-development-concept-vector-flat-graphic-design-illustration.jpg?s=612x612&w=0&k=20&c=0lpu0j-4FbuoA7xpGzt9apKZeI5F9KlNNe0qRYxjUYs=" alt="image-project" />
+        <h1>Front-End design</h1>
+      </div>
+      </a>
+      
+      
       </span>
     </main>
     </>
